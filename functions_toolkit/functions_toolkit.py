a4ff46989d0bebb70c1e0f563b6b671291785955
Zach Hornung <zachhornung@comcast.net>
2021-05-20 22:34:10 -0700

    towers of hanoi works

--- functions_toolkit/functions_toolkit.py
+++ functions_toolkit/functions_toolkit.py
@@ -24,8 +24,19 @@ def sum_series_recursive(num, a=0, b=1):
     return sum_series_recursive(num-1, a, b) + sum_series_recursive(num-2, a, b)
 
 
-def sum_series_iterative(num, a=0, b=1):
-    a,b = a,b
+def sum_series_iterative(num, starting1=0, starting2=1):
+    a,b = starting1,starting2
     for _ in range(num):
         a,b = b, a+b
     return a
+
+def tower_of_hanoi(num, from_rod, to_rod, aux_rod):
+    if num == 1:
+        print(f'\nMove disk 1 from rod {from_rod} to rod {to_rod}')
+        return
+    tower_of_hanoi(num-1, from_rod, aux_rod, to_rod)
+    print(f'\nMove disk {num} from rod {from_rod} to rod {to_rod}')
+    tower_of_hanoi(num-1, aux_rod, to_rod, from_rod)
+
+if __name__ == '__main__':
+    tower_of_hanoi(3, 'a', 'b', 'c',)
--- tests/test_functions_toolkit.py
+++ tests/test_functions_toolkit.py
@@ -1,4 +1,4 @@
-from functions_toolkit.functions_toolkit import fib_iter, fib_rec, sum_series_recursive, sum_series_iterative
+from functions_toolkit.functions_toolkit import fib_iter, fib_rec, sum_series_recursive, sum_series_iterative, tower_of_hanoi
 
 def test_fib_iter():
     actual = fib_iter(5)
@@ -16,6 +16,9 @@ def test_sum_series_recursive():
     assert actual == expected
 
 def test_sum_series_iterative():
-    actual = sum_series_iterative(5)
-    expected = 5
+    actual = sum_series_iterative(5, 2)
+    expected = 11
     assert actual == expected
+
+def test_tower_of_hanoi():
+    assert tower_of_hanoi
